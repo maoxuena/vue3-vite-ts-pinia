2e517d6c679f9124d1bb350fd0153d7935bbc3e5
maoxuena <maoxuena@foxmail.com>
2022-04-13 23:39:55 +0800

    feat: 按需引入 Naive UI

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import naive from '@/plugins/naive'
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 app.use(router)
 app.use(createPinia())
+app.use(naive)
 app.mount('#app')
