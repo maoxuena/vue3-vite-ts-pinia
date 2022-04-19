3d057af83e6558346b6b6b0edc15d6b6f008ab02
maoxuena <maoxuena@foxmail.com>
2022-04-19 22:33:02 +0800

    build: 打包

---
+++ dist/assets/IndexPage.99a54028.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./plugin-vue_export-helper.21dcd24c.js";import{o as r,c}from"./index.3ce4d04b.js";const o={};function n(t,a){return r(),c("div",null,"\u7EC4\u4EF6")}var f=e(o,[["render",n]]);export{f as default};
