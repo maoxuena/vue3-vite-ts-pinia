6b896aa5044ec5e5b56e3e470f1fa4235eb1738a
maoxuena <maoxuena@foxmail.com>
2022-04-20 09:13:32 +0800

    build: 修改打包路径

--- dist/assets/EmptyPage.0cc6cfd5.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./plugin-vue_export-helper.21dcd24c.js";import{r,o as t,n}from"./index.20200f6a.js";const c={};function _(a,s){const e=r("router-view");return t(),n(e)}var f=o(c,[["render",_]]);export{f as default};
--- dist/assets/IndexPage.35b0e244.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./plugin-vue_export-helper.21dcd24c.js";import{o as r,c as o}from"./index.20200f6a.js";const c={};function n(t,a){return r(),o("div",null,"home")}var f=e(c,[["render",n]]);export{f as default};
