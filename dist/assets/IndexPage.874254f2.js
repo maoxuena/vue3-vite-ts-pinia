5dd95cf56d09e7a6f598b6e445afe93f03a11f72
maoxuena <maoxuena@foxmail.com>
2022-04-20 09:24:23 +0800

    build: 改变打包路径

---
+++ dist/assets/IndexPage.874254f2.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./plugin-vue_export-helper.21dcd24c.js";import{o as r,c as o}from"./index.c24a838d.js";const c={};function n(t,a){return r(),o("div",null,"home")}var f=e(c,[["render",n]]);export{f as default};
