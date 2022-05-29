dc6158a03d7d94225585b38dd59817a85619e781
maoxuena <maoxuena@foxmail.com>
2022-05-29 22:35:36 +0800

    feat: 高德地图绘制点和区域

--- src/views/map/AMap/AMapPage.vue
+++ src/views/map/AMap/AMapPage.vue
@@ -2,6 +2,8 @@
   <div class="full-container full">
     <n-space class="amap-button">
       <n-button type="primary" @click="showDrawer('style')"> 主题 </n-button>
+      <n-button type="primary" @click="drawPoint"> 绘制点 </n-button>
+      <n-button type="primary" @click="drawPolygon"> 绘制区域 </n-button>
       <n-button type="primary"> 点聚合 </n-button>
       <n-button type="primary"> 运动路径 </n-button>
     </n-space>
@@ -22,7 +24,8 @@
 <script setup lang="ts">
 import AMap from 'AMap' // 引入高德地图
 import { onMounted, reactive, ref } from 'vue'
-import { pointList } from './data'
+import { pointList } from './options/point'
+import { polygonList } from './options/polygon'
 
 // 地图主题样式
 import { styleList } from './options/style'
@@ -65,6 +68,105 @@ const changeStyle = (): void => {
 const showDrawer = (value): void => {
   show.value = true
   type.value = value
+}
+
+/**
+ * 绘制点
+ */
+const drawPoint = (): void => {
+  const map = state.map
+  const data = pointList
+
+  // 信息窗口
+  const infoWindow = new AMap.InfoWindow({
+    offset: new AMap.Pixel(0, -30),
+  })
+
+  for (let i = 0; i < data.length; i++) {
+    const markerOption = new AMap.Marker({
+      position: data[i].lnglat.split(','),
+      title: data[i].name,
+      map: map,
+    })
+    markerOption.content =
+      '<div class="map-content">单位名称：' +
+      data[i].name +
+      '<br>单位地址：' +
+      data[i].addr +
+      '</div>'
+    markerOption.on('click', markerClick)
+  }
+
+  // 点击事件
+  function markerClick(e) {
+    infoWindow.setContent(e.target.content)
+    infoWindow.open(map, e.target.getPosition())
+  }
+  map.setFitView()
+}
+
+/**
+ * 绘制区域
+ */
+const drawPolygon = (): void => {
+  const map = state.map
+  const data = polygonList
+  const polygonStyle = {
+    strokeColor: '#2b8cbe', // 线颜色
+    strokeOpacity: 1, // 线透明度
+    strokeWeight: 3, // 线宽
+    strokeStyle: 'dashed', // 线样式
+    strokeDasharray: [5, 5],
+    fillColor: '#ccebc5', // 填充色
+    fillOpacity: 0.5, // 填充透明度
+  }
+  data.forEach((item) => {
+    // 处理边界经纬度
+    const tempArr = item.latiAndLongi.split(';')
+    const path = []
+    for (let i = 0; i < tempArr.length; i++) {
+      if (tempArr[i].trim() === '' || tempArr === null) {
+        continue
+      }
+      const t = tempArr[i].split(',')
+      path.push(t)
+    }
+    // 创建多边形（Polygon）实例
+    const polygon = new AMap.Polygon({
+      path: path,
+      content: '<div class="custom-content-label">' + item.Name + '</div>', //自定义点标记覆盖物内容
+      ...polygonStyle,
+    })
+    // 鼠标移入
+    polygon.on('mouseover', () => {
+      polygon.setOptions({
+        fillOpacity: 0.7,
+        fillColor: '#7bccc4',
+      })
+    })
+    // 鼠标移出
+    polygon.on('mouseout', () => {
+      polygon.setOptions({
+        fillOpacity: 0.5,
+        fillColor: '#ccebc5',
+      })
+    })
+
+    // 处理中心点经纬度
+    const center = item.center.split(',')
+    // 创建标记点（Marker）实例
+    const marker = new AMap.Marker({
+      map,
+      position: [center[0], center[1]], //基点位置
+      offset: new AMap.Pixel(0, 0), //相对于基点的偏移位置
+      draggable: false, //是否可拖动
+      icon: new AMap.Icon({ image: '' }),
+      content: '<div class="custom-content-label">' + item.name + '</div>', //自定义点标记覆盖物内容
+    })
+    // 添加多边形、标记点
+    map.add(polygon)
+    map.add(marker)
+  })
 }
 
 onMounted(() => {
