cff3463b39beb0a0a2b84123039f0b6a4233deae
maoxuena <maoxuena@foxmail.com>
2022-07-15 13:23:05 +0800

    build: 打包

---
+++ dist/assets/ModalCompPage.5de04960.js
@@ -0,0 +1,1 @@
+import{_ as a,o as e,g as r}from"./index.9a0ced8a.js";var o=a({},[["render",function(a,o){return e(),r("div",null,"NaiveUi-ModalComp")}]]);export{o as default};
---
+++ dist/assets/useCurrentInstance.8edf2477.js
@@ -0,0 +1,1 @@
+import{m as o}from"./index.9a0ced8a.js";function r(){const{appContext:r}=o();return{proxy:r.config.globalProperties}}export{r as u};
